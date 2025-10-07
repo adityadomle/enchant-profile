88753402373a1d2fe94f97d4c72fddc093326cc9
Aditya Domle <adityadomle14@gmail.com>
2025-10-07 21:29:37 +0530

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,36 @@
+export const siteConfig = {
+  name: 'abhishekg.com.np',
+  url: 'https://abhishekg.com.np',
+  description: 'Portfolio of Abhishek Ghimire',
+  keywords: [
+    'Abhishek Ghimire',
+    'Abhishek G',
+    'abhishekg.com.np',
+    'Abhishek Ghimire Portfolio',
+    'Abhishek Ghimire Projects',
+    'Abhishek Ghimire Skills',
+    'Abhishek Ghimire Experience',
+    'Abhishek Ghimire Education',
+    'Abhishek Ghimire Contact',
+    'Abhishek Ghimire Blog',
+    'Abhishek Ghimire Resume',
+  ],
+  links: {
+    shadcn: 'https://ui.shadcn.com/',
+    telegram: 'https://t.me/cypherab01',
+    twitter: 'https://x.com/cypherab01',
+    github: 'https://github.com/cypherab01/abhishekg.com.np',
+    githubProfile: 'https://github.com/cypherab01',
+    resume: 'https://www.overleaf.com/read/pwbbqtpthxxr#946f81',
+    email: 'mailto:aghimire074@gmail.com',
+    githubUsername: 'cypherab01',
+  },
+  bitinfonepal: 'https://bitinfonepal.com',
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b',
+};
